a3c8c6741000141a7e69bc65572c094bdb44df05
pppriya <priyanshi15633@gmail.com>
2025-08-01 19:11:49 +0530

    Binary search program

---
+++ src/main/java/binarySearch.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,5,7,19,33,263,453,983};
+        int target = 7;
+        System.out.println(searchElement(arr, target, 0, arr.length-1));
+    }
+
+    public static int searchElement(int[] arr, int target, int start, int end){
+        if(start >  end)return -1;
+        int mid = (end+start)/2;
+        if(arr[mid] == target)return mid;
+        if(target < arr[mid])
+            return searchElement(arr, target, start, mid-1);
+        return searchElement(arr, target, mid+1, end);
+    }
+}
